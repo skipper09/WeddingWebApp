ab85c524928dadf4ddad8638a2b8f67a2f3f204d
nituroy7 <nitu_prodhani@yahoo.com>
2017-08-24 02:01:10 -0500

    fixes

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
     var slidesWrapper = $('.cd-hero-slider');
-
     //check if a .cd-hero-slider exists in the DOM 
     if (slidesWrapper.length > 0) {
         var primaryNav = $('.cd-primary-nav'),
@@ -10,18 +9,14 @@ jQuery(document).ready(function($) {
             visibleSlidePosition = 0,
             autoPlayId,
             autoPlayDelay = 5000;
-
         //upload videos (if not on mobile devices)
         uploadVideo(slidesWrapper);
-
         //autoplay slider
         setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-
         //on mobile - open/close primary navigation clicking/tapping the menu icon
         primaryNav.on('click', function(event) {
             if ($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
         });
-
         //change visible slide
         sliderNav.on('click', 'li', function(event) {
             event.preventDefault();
@@ -30,16 +25,13 @@ jQuery(document).ready(function($) {
                 // if it's not already selected
                 var selectedPosition = selectedItem.index(),
                     activePosition = slidesWrapper.find('li.selected').index();
-
                 if (activePosition < selectedPosition) {
                     nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
                 } else {
                     prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
                 }
-
                 //this is used for the autoplay
                 visibleSlidePosition = selectedPosition;
-
                 updateSliderNavigation(sliderNav, selectedPosition);
                 updateNavigationMarker(navigationMarker, selectedPosition + 1);
                 //reset autoplay
@@ -47,38 +39,31 @@ jQuery(document).ready(function($) {
             }
         });
     }
-
     function nextSlide(visibleSlide, container, pagination, n) {
         visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
             visibleSlide.removeClass('is-moving');
         });
-
         container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
         checkVideo(visibleSlide, container, n);
     }
-
     function prevSlide(visibleSlide, container, pagination, n) {
         visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
             visibleSlide.removeClass('is-moving');
         });
-
         container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
         checkVideo(visibleSlide, container, n);
     }
-
     function updateSliderNavigation(pagination, n) {
         var navigationDot = pagination.find('.selected');
         navigationDot.removeClass('selected');
         pagination.find('li').eq(n).addClass('selected');
     }
-
     function setAutoplay(wrapper, length, delay) {
         if (wrapper.hasClass('autoplay')) {
             clearInterval(autoPlayId);
             autoPlayId = window.setInterval(function() { autoplaySlider(length) }, delay);
         }
     }
-
     function autoplaySlider(length) {
         if (visibleSlidePosition < length - 1) {
             nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
@@ -90,7 +75,6 @@ jQuery(document).ready(function($) {
         updateNavigationMarker(navigationMarker, visibleSlidePosition + 1);
         updateSliderNavigation(sliderNav, visibleSlidePosition);
     }
-
     function uploadVideo(container) {
         container.find('.cd-bg-video-wrapper').each(function() {
             var videoWrapper = $(this);
@@ -104,21 +88,17 @@ jQuery(document).ready(function($) {
             }
         });
     }
-
     function checkVideo(hiddenSlide, container, n) {
         //check if a video outside the viewport is playing - if yes, pause it
         var hiddenVideo = hiddenSlide.find('video');
         if (hiddenVideo.length > 0) hiddenVideo.get(0).pause();
-
         //check if the select slide contains a video element - if yes, play the video
         var visibleVideo = container.children('li').eq(n).find('video');
         if (visibleVideo.length > 0) visibleVideo.get(0).play();
     }
-
     function updateNavigationMarker(marker, n) {
         marker.removeClassPrefix('item').addClass('item-' + n);
     }
-
     $.fn.removeClassPrefix = function(prefix) {
         //remove all classes starting with 'prefix'
         this.each(function(i, el) {
@@ -129,12 +109,9 @@ jQuery(document).ready(function($) {
         });
         return this;
     };
-
     $("#providerSubmit").on("submit", function() {});
-
     $("#registerPro").click(function(e) {
         e.preventDefault();
-
         var data = {};
         data.serviceid = $("#serviceid").val();
         data.company = $("#company").val();
@@ -144,7 +121,6 @@ jQuery(document).ready(function($) {
         data.phone = $("#phone").val();
         data.Address1 = $("#Address1").val();
         data.zipCode = $("#zipCode").val();
-
         $.ajax({
             url: 'http://localhost:8080/api/provider/create',
             method: "POST",
