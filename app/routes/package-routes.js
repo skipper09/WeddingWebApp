128adbcd05c52ca2718401d066a4fc66f228c4cf
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-08-30 18:29:18 -0500

    Adding leritha's changes

--- app/routes/package-routes.js
+++ app/routes/package-routes.js
@@ -3,26 +3,43 @@ var db = require("../models");
 module.exports = function (app) {
 
     app.get("/package", function (req, res) {
-        console.log('something');
-        db.Packages.findAll({})
-            .then(function (results) {
-                var hbsObject = {
-                    package: results
-                };
-                res.render("packagehbs", hbsObject);
-            });
+        db.Packages.findAll({
+            include: ['venue', 'photographer', 'music', 'florist', 'caterer', 'cake'],
+        }).then(function (results) {
+            var price = 0;
+            for (var i=0; i < results.length; i++) {
+                if (results[i].venue)
+                    price += results[i].venue.price;
+                if (results[i].photographer)
+                    price += results[i].photographer.price;
+                if (results[i].caterer)
+                    price += results[i].caterer.price;
+                if (results[i].cake)
+                    price += results[i].cake.price;
+                if (results[i].music)
+                    price += results[i].music.price;
+                if (results[i].florist)
+                    price += results[i].florist.price;
+            }
+
+            var hbsObject = {
+                package: results,
+                price: price
+            };
+            res.render("packagehbs", hbsObject);
+        });
     });
 
     app.post("/package/create", function (req, res) {
         console.log(res);
 
         db.Packages.create({
-            venue: req.body.venue,
-            photographers: req.body.photographer,
-            caterers: req.body.caterers,
-            cake: req.body.cake,
-            music: req.body.music,
-            florist: req.body.florist
+            venueId: req.body.venue,
+            photographerId: req.body.photographer,
+            catererId: req.body.caterers,
+            cakeId: req.body.cake,
+            musicId: req.body.music,
+            floristId: req.body.florist
         })
             .then(function (result) {
                 res.redirect("/package");
